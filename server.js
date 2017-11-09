ca42fcb48b20f709816bbd826dd592a14ae0c3dd
kwlee <keith.won.lee@gmail.com>
2017-11-08 16:41:11 -0800

    started epic button game

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var session = require('express-session');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: 'codingdojorocks'}));  
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    if(!req.session.count){
+        req.session.count = 0;
+    }
+    res.render("index", { counter: req.session.count });
+})
+
+app.listen(5000,function(){
+    console.log("listening on port 5000");
+})
